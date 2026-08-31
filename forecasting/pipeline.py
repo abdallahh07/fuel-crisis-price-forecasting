0009378429baf2deeaa530697e5428a419640539
abdallahh07 <abdallahhashad029@gmail.com>
2026-08-31 16:15:51 +0300

    adding pipeline

---
+++ forecasting/pipeline.py
@@ -0,0 +1,32 @@
+from sklearn.linear_model import Lasso
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+
+from data_manager import load_merge_data
+from features import create_x_and_y
+from config import settings
+
+
+class ForecastingPipeline:
+    def __init__(self):
+        self.model = None
+        self.x_train = None
+        self.x_test = None
+        self.y_train = None
+        self.y_test = None
+
+    def run(self):
+        master = load_merge_data()
+        self.x_train, self.x_test, self.y_train, self.y_test = create_x_and_y(master)
+
+        self.model = Pipeline([
+            ("scaler", StandardScaler()),
+            ("model", Lasso(
+                alpha=settings["model"]["alpha"],
+                max_iter=settings["model"]["max_iter"]
+            ))
+        ])
+
+        self.model.fit(self.x_train, self.y_train)
+
+        return self.model
