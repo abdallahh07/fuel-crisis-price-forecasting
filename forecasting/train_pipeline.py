6b4ec70bcce3a8cecc0a0b23258dc3e62758126d
abdallahh07 <abdallahhashad029@gmail.com>
2026-08-31 16:20:29 +0300

    add train_pipeline

---
+++ forecasting/train_pipeline.py
@@ -0,0 +1,14 @@
+from pipeline import ForecastingPipeline
+from config import settings
+import joblib
+
+def train():
+    pipeline = ForecastingPipeline()
+    pipeline.run()
+
+    joblib.dump(pipeline.model, settings["artifacts"]["model_path"])
+
+    return pipeline.model
+
+if __name__ == "__main__":
+    train()
