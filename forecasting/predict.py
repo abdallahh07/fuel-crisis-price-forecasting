a23ac34f652ad75e999f500949c3664652aa4a97
abdallahh07 <abdallahhashad029@gmail.com>
2026-08-31 16:22:25 +0300

    add predict.py

---
+++ forecasting/predict.py
@@ -0,0 +1,13 @@
+import joblib
+from config import settings
+
+def load_model():
+    model = joblib.load(settings["artifacts"]["model_path"])
+    return model
+  
+def predict(new_data):
+    model = load_model()
+    predictions = model.predict(new_data)
+    return predictions
+  
+  
